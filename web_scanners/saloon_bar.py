f62c0bd7c3037af72b6ee74d384d63e51d7f0ffd
Sasha <spavelovich@lexipedium.org>
2014-08-06 23:02:09 -0400

    Adding word frequency logging functionality

--- web_scanners/saloon_bar.py
+++ web_scanners/saloon_bar.py
@@ -3,39 +3,45 @@
 import requests
 import time
 import datetime
- 
+
 def saloonBarReq():
 	reqData = {"action" : "raw"}
 	reqHeaders = {"User-Agent":"Saloon Bar scanner: Sokolovskaya, Checking Rational Wiki Saloon Bar for evidence of harassment, sokolovskaya.akula.sturmkrieg.ru in Dallas, Texas"}
 	req = requests.post("http://rationalwiki.org/wiki/RationalWiki:Saloon_bar", data=reqData, headers=reqHeaders)
 	req.encoding = "utf-8"
 	return req.content
- 
+
 req = saloonBarReq()
- 
+count = [0] * 20
+
 def checkWord(word):
+	i=0
 	if ( word in req.lower() ):
-		print word.upper() +": TRUE"
+		occr = req.lower().count(word)
+		print word.upper() +": TRUE "+ str(occr)
+		count[i] = occr
 		return True
 	else:
 		print word.upper() +": FALSE"
 		return False
- 
-wordList = ["ehrenstein", " ie ", "inquisitor", "sasha", "lieutenant s", "reznov", "dondrekhan", "dewey", "nazi", "rape"]
- 
+	i = i + 1
+
+wordList = ["ehrenstein", " ie ", "inquisitor", "sasha", "the", "lieutenant s", "reznov", "dondrekhan", "dewey", "nazi", "rape", "arcane", "arcane21"]
+
 while True:
 	t = time.localtime()
 	timestamp = "\n"+ time.asctime(t)
 	print timestamp
 	file = open("saloonbar_log.txt", 'a')
-	text = "\n\n"+ timestamp +"\n"+ req +""
+	text = "\n\n"+ timestamp +"\n"+ req +"\n\n"
 	file.write(text)
 	file.close()
 	for word in wordList:
+		i = 0
 		check = checkWord(word)
 		if ( check == True ):
 			file = open("saloonbar_log.txt", 'a')
-			text = word.upper() +": TRUE\n"
+			text = word.upper() +": TRUE "+ str(count[i]) +"\n"
 			file.write(text)
 			file.close()
 		else:
@@ -43,4 +49,6 @@ while True:
 			text = word.upper() +": FALSE\n"
 			file.write(text)
 			file.close()
+		i = i + 1
 	time.sleep(600)
+
